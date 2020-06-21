bf5e553e5734b9aa1796aa7241bcb8ca9c884f8a
Stuart Bowes <stuartbowes2000@gmail.com>
2020-06-21 10:12:35 +0100

    Replace ProductRepository with a generic repository

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,7 +27,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
         }
