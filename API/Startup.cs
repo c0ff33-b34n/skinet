e74434fcd133005e5c925125c31a0c430869cb64
Stuart Bowes <stuartbowes2000@gmail.com>
2020-06-30 10:24:03 +0100

    Set up Identity to use the Jwt token

--- API/Startup.cs
+++ API/Startup.cs
@@ -49,7 +49,7 @@ namespace API
             });
 
             services.AddApplicationServices();
-            services.AddIdentityServices();
+            services.AddIdentityServices(_config);
             services.AddSwaggerDocumentation();
             services.AddCors(opt => 
             {
@@ -76,6 +76,7 @@ namespace API
 
             app.UseCors("CorsPolicy");
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseSwaggerDocumentation();
