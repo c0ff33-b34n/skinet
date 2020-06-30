96fdb410dad34a2b870acce3c6dc6928a85513fc
Stuart Bowes <stuartbowes2000@gmail.com>
2020-06-30 13:22:25 +0100

    Add validation for registering an account in API

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -93,6 +93,12 @@ namespace API.Controllers
     [HttpPost("register")]
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
+        if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+        {
+            return new BadRequestObjectResult(new ValidationErrorResponse{
+                Errors = new []{"Email address is in use"}});
+        }
+
         var user = new AppUser
         {
             DisplayName = registerDto.DisplayName,
