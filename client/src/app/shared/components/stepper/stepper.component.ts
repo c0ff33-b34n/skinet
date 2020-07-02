95bea384078f3619dc894aaf0ab371294b8fa06f
Stuart Bowes <stuartbowes2000@gmail.com>
2020-07-02 14:40:43 +0100

    Set up and style the stepper component

--- client/src/app/shared/components/stepper/stepper.component.ts
+++ client/src/app/shared/components/stepper/stepper.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CdkStepper } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-stepper',
   templateUrl: './stepper.component.html',
-  styleUrls: ['./stepper.component.scss']
+  styleUrls: ['./stepper.component.scss'],
+  providers: [{provide: CdkStepper, useExisting: StepperComponent}]
 })
-export class StepperComponent implements OnInit {
-
-  constructor() { }
+export class StepperComponent extends CdkStepper implements OnInit {
+  @Input() linearModeSelected: boolean;
 
   ngOnInit() {
+    this.linear = this.linearModeSelected; // check if previous steps in form should be completed before progress.
+  }
+
+  onClick(index: number) {
+    this.selectedIndex = index;
+    console.log(this.selectedIndex);
   }
 
 }
