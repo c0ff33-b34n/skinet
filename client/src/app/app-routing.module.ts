d191da86c523efd389832bcec5082d21cd496fde
Stuart Bowes <stuartbowes2000@gmail.com>
2020-07-07 11:55:14 +0100

    Add Order Module to client and track down and fix bug.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,21 +6,31 @@ import { ServerErrorComponent } from './core/server-error/server-error.component
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { AuthGuard } from './core/guards/auth.guard';
 
-
 const routes: Routes = [
-  {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
-  {path: 'test-error', component: TestErrorComponent, data: {breadcrumb: 'Test Errors'}},
-  {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'}},
-  {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'}},
-  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: {breadcrumb: 'Shop'}},
-  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: {breadcrumb: 'Basket'}},
-  {path: 'checkout',
+  { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
+  { path: 'test-error', component: TestErrorComponent, data: { breadcrumb: 'Test Errors' } },
+  { path: 'server-error', component: ServerErrorComponent, data: { breadcrumb: 'Server Error' } },
+  { path: 'not-found', component: NotFoundComponent, data: { breadcrumb: 'Not Found' } },
+  { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: { breadcrumb: 'Shop' } },
+  { path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: { breadcrumb: 'Basket' } },
+  {
+    path: 'checkout',
     canActivate: [AuthGuard],
-   loadChildren: () => import('./checkout/checkout.module')
-    .then(mod => mod.CheckoutModule), data: {breadcrumb: 'Checkout'}},
-  {path: 'account', loadChildren: () => import('./account/account.module')
-    .then(mod => mod.AccountModule), data: {breadcrumb: {skip: true}}}, // skip adding a breadcrumb for this path.
-  {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
+    loadChildren: () => import('./checkout/checkout.module')
+      .then(mod => mod.CheckoutModule), data: { breadcrumb: 'Checkout' }
+  },
+  {
+    path: 'orders',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./orders/orders.module')
+      .then(mod => mod.OrdersModule), data: { breadcrumb: 'Orders' }
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./account/account.module')
+      .then(mod => mod.AccountModule), data: { breadcrumb: { skip: true } }
+  },
+  { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
 ];
 
 @NgModule({
